573c3fb2c610c3822e86395fbf920ae679637078
MUHSIN K <muhzi4u@gmail.com>
2018-04-10 12:42:23 +0530

    fix: update bug in home page

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ const App = () => (
       <img src={logo} className="App-logo" alt="logo" />
       <h2>Welcome to React Starter</h2>
     </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
   </div>
 );
 
